fe92e8a364ee3acf2d29568ddfabf1568684a9a7
DESKTOP-HHVMK3F\shqhk <wjdtmd789@gmail.com>
2021-06-18 18:56:08 +0900

    [정승현] 0618 commit

--- naverdev/static/js/subMenu.js
+++ naverdev/static/js/subMenu.js
@@ -3,7 +3,10 @@
 */
 
 $(function(){ // subMenu function() start
-   $('#nav_index li').hover(function(){ // 각 menuItem 이 hover 이벤트를 받을 경우
+    var toggle = false;
+    nav_index = 1;
+    var w_btn = "";
+    $('#nav_index li').hover(function(){ // 각 menuItem 이 hover 이벤트를 받을 경우
         index = $('#nav_index li').index(this); // hover 이벤트가 발생한 index 를 찾는다.
         if(index <= 5 && index > 0){ // Products ~ Support 까지의 hover 인덱스만 필요하므로 필터링
             sub_index = index;
@@ -31,7 +34,7 @@ $(function(){ // subMenu function() start
                 var default_value = 50 - (submenu_count * 5); // margin-left 를 재구성 해주기 위한 default_value
                 $('#'+sub_index_name).children('div:nth-child(1)').css("margin-left", default_value + '%')
                 for(var i=1;i<submenu_count;i++){ // 자식 요소의 개수만큼 margin-left 재구성
-                $('#'+sub_index_name).children('div:nth-child('+(i+1)+')').css("margin-left", + (default_value + (i*10)) +'%')
+                $('#'+sub_index_name).children('div:nth-child('+(i+1)+')').css("margin-left", + (default_value + (i*12)) +'%')
                 }
             }
             $('.subMenuBox').css('display','none'); // sub_menu가 켜져있을 시 먼저 close
@@ -41,8 +44,44 @@ $(function(){ // subMenu function() start
             $('#'+sub_index_name).mouseleave(function(){
                 $('.subMenuBox').css('display','none'); // sub_menu close
             });
-        });
-   $('#nav_toggle').click(function(){
-        $('#react-subMenu').css('display','block');
-   });
+    });
+
+    $('#nav_toggle').click(function(){
+        if(!toggle){
+            $('#react-subMenu').css('display','block');
+            $('#react-subMenuItem'+nav_index).css('display','block');
+            toggle = true;
+        }else{
+            $('#react-subMenu').css('display','none');
+            toggle = false;
+        }
+    });
+
+    $('.btn_prev').click(function(){
+        w_btn = 'prev';
+        showSubMenu(nav_index, w_btn)
+    });
+
+    $('.btn_next').click(function(){
+        w_btn = 'next';
+        showSubMenu(nav_index, w_btn)
+    });
+
+    function showSubMenu(nav_index, w_btn){
+        $('#react-subMenuItem'+nav_index).css('display','none');
+        if(w_btn == 'prev'){
+            this.nav_index = this.nav_index - 1;
+            if(this.nav_index < 1){
+                this.nav_index = 6;
+            }
+            $('#react-subMenuItem'+this.nav_index).css('display','block');
+        }else{
+            this.nav_index = this.nav_index + 1;
+            if(this.nav_index > 6){
+                this.nav_index = 1;
+            }
+            $('#react-subMenuItem'+this.nav_index).css('display','block');
+        }
+    }
 }); //$(function() 끝
+
