e857cb4cbe18056724f93570fa519f3044a497d1
DESKTOP-HHVMK3F\shqhk <wjdtmd789@gmail.com>
2021-06-18 19:45:52 +0900

    [정승현] 0618 commit & push

--- config/settings.py
+++ config/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # 로그인 성공 후 이동하는 URL
-LOGIN_REDIRECT_URL = '/board/'
-LOGOUT_REDIRECT_URL = '/board/'
+LOGIN_REDIRECT_URL = '/index/'
+LOGOUT_REDIRECT_URL = '/index/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
